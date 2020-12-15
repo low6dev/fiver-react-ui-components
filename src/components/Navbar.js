fcc8a6a518aba788e7b044d720ff8954d34e06f7
Joe Harrison <joeh@low6.co.uk>
2020-12-15 23:28:01 +0000

    testing

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+  background: red;
+  width: 300%;
+  height: 70px;
+`;
+
+const Navbar = () => {
+  return <StyledWrapper>Navbar</StyledWrapper>;
+};
+
+export default Navbar;
